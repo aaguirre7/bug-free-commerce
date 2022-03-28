dac22f3c1abdd4a9c27e1988e17ccb8a30924c4c
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-03-28 01:48:38 -0400

    completed code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,31 @@
+// imports important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// imports our database connection from config.js
 const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
-
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
